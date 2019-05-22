251ff8017ecb2920d360fca1ae803e856be33599
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-22 04:02:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,8 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(x)
-    sql = <<-SQL 
-      SELECT * 
+    sql = <<-SQL
+      SELECT *
       FROM students
       WHERE students.grade = 10
       LIMIT ?
@@ -69,7 +69,7 @@ class Student
     end
   end
 
-  
+
 
   def save
     sql = <<-SQL
